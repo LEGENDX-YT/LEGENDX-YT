942515c0b221634f532db74da572755ea7d9f84a
【★ʟɛɢɛռɖ ✘ Hคckeℝ  】 <104726886+LEGENDX-YT@users.noreply.github.com>
2023-02-10 07:12:31 -0800

    Create prawash.py

---
+++ prawash.py
@@ -0,0 +1,17 @@
+from `legendx` import legend-yt
+from program import server 
+from telepy
+from pyrogram 
+from legend-x
+from github
+from clients
+
+ def program()
+         github(:"SESSION_STRING_PYROGRAM":).print
+          github(os.eval.program). id
+       sender_message(:-).print
+        print(program)await if legendserver_
+        legend("legend_session").id if server not runner
+        
+def legend_os() patter/start/program/server/os/import=key_words
+    key_prosess(os{vars}).opne
