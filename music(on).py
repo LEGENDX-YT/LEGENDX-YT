98e8e60f92b5bca18708c776a8b41b82ff103f8a
【★ʟɛɢɛռɖ ✘ Hคckeℝ  】 <104726886+LEGENDX-YT@users.noreply.github.com>
2023-02-11 08:17:12 -0800

    Create music(on).py

---
+++ music(on).py
@@ -0,0 +1,26 @@
+import os
+import music_gaana(os)
+import legend(music_os).opne __music__.py
+from telethon(__Pyrogram__)plugins.loding(os)
+print(os).loading("PYROGRAM INSTALL FASTING OS")
+telethon.plugins{var}(.os.eval.plugins)(:-).close
+class in=os 
+  class (plugins_system) install.(await)
+      await = install(:-){var, added, instal, ping, start, help
+        close()
+          install()
+             def program()                      
+                  plugins("install program system reboot /start play songs.")
+                  install(?start).in plugins.restart
+                  install("sec plugins install..")
+                  close()
+ import system
+ import telegram 
+     app()close(restart.(/off).await close if not class patter(.pin))
+                          def os 
+                           def import
+                            def id 
+                              def program
+                                def import=(app_Clients)
+                                  def i=not
+                          session(close.restart)():print(os).class
