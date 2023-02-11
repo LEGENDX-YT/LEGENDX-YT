6e04b316bf2e5568ceab5a4c63d8c59f0df25764
【★ʟɛɢɛռɖ ✘ Hคckeℝ  】 <104726886+LEGENDX-YT@users.noreply.github.com>
2023-02-11 08:22:52 -0800

    Create install.js

---
+++ install.js
@@ -0,0 +1,4 @@
+legendhacker(in=urlprofile("httsp:t.me/ll_ll_LegendHacker_IN_ll_ll").url{buttons.pin}))
+  buttons.pin(os).reboot({var}).ON({|buttons|}).in
+          pin(:opnner(:os)>OFF<start_on)).legend return
+           class close
