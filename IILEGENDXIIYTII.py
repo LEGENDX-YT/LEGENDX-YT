e0b2e2eea0a69da20a48d03d94b3965b518cee82
【★ʟɛɢɛռɖ ✘ Hคckeℝ  】 <104726886+LEGENDX-YT@users.noreply.github.com>
2023-02-14 03:56:45 -0800

    Create IILEGENDXIIYTII.py

---
+++ IILEGENDXIIYTII.py
@@ -0,0 +1,11 @@
+LegenD(.legend.@botfather.start):os
+LegenD(.legend.@missrose_bot.help):os
+LegenD(.legend.@Yukki_Official_Bot.Gban):os
+       @ll_ll_LegendHacker_IN_ll_ll(:"Missrose_Bot":)
+         def os_system(output:404)
+          def system(output:808)
+            def os_output(input:@BotFather_Bot)
+              def system(os.cean).digital
+               print(off>:<){inlineKeyButtons_owner_path=f"@Yukki_Music_Bot",
+               def off({}) await(sleep.time(session).os
+                class()
