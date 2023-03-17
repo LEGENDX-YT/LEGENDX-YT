55a258e974de82a505522df56fce1e0de13411d3
【★ʟɛɢɛռɖ ✘ Hคckeℝ  】 <104726886+LEGENDX-YT@users.noreply.github.com>
2023-03-16 22:02:50 -0700

    Create webDown(listed).py

---
+++ webDown(listed).py
@@ -0,0 +1,13 @@
+from Linode.com("url=https://login.linode.com/welcome/review")/url(clear_mode(with).cc
+from system.os
+from os system_team
+from legendx import telegramClients
+from system_import=("Clients").cc
+    system(>os<).printers(.filters.user()))
+     & filters.user()
+     & filters.sudo()
+     & filters.owner_id()
+     & filters.owner(clear)
+clear("LegendX").cline(/on?).patter
+clear("filters").os opner
+url.buttons(:with(if=Linode)):await program.command<if<with,await))
